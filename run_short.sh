e5975e95130a6b28c615fb6bc2f97082eaf2a0bd
Ubuntu <ubuntu@ip-172-31-50-140.ec2.internal>
2021-06-04 15:05:23 +0000

    add run scripts

---
+++ run_short.sh
@@ -0,0 +1,40 @@
+
+#### REAL RUNS ON BENCHMARK DATA
+mkdir -p short_runs
+
+# Run the conll languages
+for lang in eng deu esp ned
+do
+    echo
+    echo DO $lang
+
+    batch_size=8
+    model_name="bert-base-multilingual-cased"
+    if [ $lang == "eng" ]
+    then
+        model_name="roberta-base"
+    fi 
+
+    echo "python main.py -dd data/conll2003/${lang}_P-1000_short -cd save_${lang}_P-1000_short -rd resource --model-name $model_name --batch_size $batch_size -en 50 &> runs/21-06-01_run_${lang}_P-1000_short.log"
+    python main.py -dd data/conll2003/${lang}_P-1000_short -cd save_${lang}_P-1000_short -rd resource --model-name $model_name --batch_size $batch_size -en 50 &> runs/21-06-01_run_${lang}_P-1000_short.log
+
+done
+
+
+# Run the ontonotes languages
+for lang in english chinese arabic
+do
+    echo
+    echo DO $lang
+
+    batch_size=8
+    model_name="bert-base-multilingual-cased"
+    if [ $lang == "english" ]
+    then 
+        model_name="roberta-base"
+    fi 
+    echo "python main.py -dd data/ontonotes5/${lang}_P-1000_short -cd save_${lang}_P-1000_short -rd resource --model-name $model_name --batch_size $batch_size -en 50 &> runs/21-06-01_run_${lang}_P-1000_short.log"
+    python main.py -dd data/ontonotes5/${lang}_P-1000_short -cd save_${lang}_P-1000_short -rd resource --model-name $model_name --batch_size $batch_size -en 50 &> runs/21-06-01_run_${lang}_P-1000_short.log
+
+
+done
